add7632b1a944358ed8baefde021a0382deff2b6
jrry8 <31778235+jrry8@users.noreply.github.com>
2023-08-15 16:37:47 -0700

    init 48

---
+++ 48.py
@@ -0,0 +1,37 @@
+# Self Powers
+
+import unittest
+
+def sumLastDigits(x, y):
+    return 0
+
+def squareLastDigits(x):
+    return 0
+
+def multLastDigits(x, y):
+    return 0
+
+def powLastDigits(x, n):
+    return 0
+
+
+
+class TestSum(unittest.TestCase):
+    #TODO
+    def test(self):
+        self.assertEqual(0,0)
+
+class TestSquare(unittest.TestCase):
+    #TODO
+    def test(self):
+        self.assertEqual(0,0)
+
+class TestMult(unittest.TestCase):
+    #TODO
+    def test(self):
+        self.assertEqual(0,0)
+
+class TestPow(unittest.TestCase):
+    #TODO
+    def test(self):
+        self.assertEqual(0,0)
