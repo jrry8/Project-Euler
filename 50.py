6651433b3086ccc04928288204b20ebcff5871ec
jrry8 <31778235+jrry8@users.noreply.github.com>
2023-08-16 15:17:21 -0700

    init for 26 and 50

---
+++ 50.py
@@ -0,0 +1,14 @@
+# for x in range(1000000)
+#   if x is prime
+#       find longest sum of consec primes
+
+memo = dict()
+seenPrimes = set()
+
+def isPrime(x):
+    return False
+
+def longestPrimeSum(x):
+    if x not in memo:
+        #TODO
+    return memo[x]
