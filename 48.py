1d568ba2114a0850933675ccdabb292aec601920
jrry8 <31778235+jrry8@users.noreply.github.com>
2023-08-15 17:03:18 -0700

    added some test cases and implementation

--- 48.py
+++ 48.py
@@ -1,30 +1,48 @@
 # Self Powers
+# Find the last ten digits of the series
+# 1**1 + 2**2 + ... + 1000**1000
 
 import unittest
 
-def sumLastDigits(x, y):
-    return 0
+TRUNC = 10**10
 
-def squareLastDigits(x):
-    return 0
+def sumLastDigits(x, y):
+    x %= TRUNC
+    y %= TRUNC
+    return (x+y) % TRUNC
 
 def multLastDigits(x, y):
+    x %= TRUNC
+    y %= TRUNC
+    ans = 0
+    #TODO
     return 0
 
 def powLastDigits(x, n):
-    return 0
+    if n == 1:
+        return x % TRUNC
+    tmp = multLastDigits(x, x)
+    ans = powLastDigits(tmp, n // 2)
+    if n % 2 == 1:
+        ans = multLastDigits(ans, x)
+    return ans % TRUNC
 
-
+def foo():
+    ans = 0
+    for i in range(1, 1001):
+        cur = powLastDigits(i, i)
+        ans = sumLastDigits(ans, cur)
+    return ans
 
 class TestSum(unittest.TestCase):
-    #TODO
-    def test(self):
-        self.assertEqual(0,0)
+    def testFewDigits(self):
+        self.assertEqual(sumLastDigits(0,0), 0)
+        self.assertEqual(sumLastDigits(142, 5643), 5785)
+        self.assertEqual(sumLastDigits(7,11), 18)
 
-class TestSquare(unittest.TestCase):
-    #TODO
     def test(self):
-        self.assertEqual(0,0)
+        self.assertEqual(sumLastDigits(902834012353, 78329181114), 1163193467)
+        self.assertEqual(sumLastDigits(19834756293, 97465928374), 7300684667)
 
 class TestMult(unittest.TestCase):
     #TODO
@@ -35,3 +53,5 @@ class TestPow(unittest.TestCase):
     #TODO
     def test(self):
         self.assertEqual(0,0)
+
+unittest.main()
