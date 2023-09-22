bd867c0be70c50a58e836da3e05152b5fda76256
jrry8 <31778235+jrry8@users.noreply.github.com>
2023-09-22 02:54:36 -0700

    added file reading

--- 79.py
+++ 79.py
@@ -1,2 +1,9 @@
 # construct DAG
 
+logins = []
+with open("0079_keylog.txt") as f:
+    for line in f:
+        logins.append([line[0], line[1]])
+        logins.append([line[1], line[2]])
+
+print(logins)
